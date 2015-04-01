6eb19fe98528108791f1869027677acc0a06dba1
Taylor Smith <tsmith@thebhwgroup.com>
2015-04-01 15:47:10 -0500

    files from canvas untouched

---
+++ hw6/StatefulMonadTest.hs
@@ -0,0 +1,18 @@
+import Stateful hiding (Stateful, evaluate, execute)
+import Base
+import StatefulMonad 
+import StatefulParse
+
+t1 = parseExp ("var x = mutable 3;"++
+     "var y = mutable true;"++
+     "if (@y) { x = @x + 1 } else { x };"++
+     "@x")
+
+t2 = parseExp ("var x = mutable 3;"++
+     "var y = mutable 7;"++
+     "x = @x + @y;"++
+     "y = @y * @x")
+
+main = do
+  test "evaluate" execute t1
+  test "evaluate" execute t2
